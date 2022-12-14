20f1f36e85ac4228b89a49f336d6fa15e0adcfec
Ashwani Rajput <ashwanir461@gmail.com>
2022-12-14 14:13:01 +0530

    done with the work

---
+++ APIGatewayDemo/src/test/java/com/example/APIGatewayDemo/ApiGatewayDemoApplicationTests.java
@@ -0,0 +1,12 @@
+package com.example.APIGatewayDemo;
+
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiGatewayDemoApplicationTests {
+
+	//	@Test
+	void contextLoads() {
+	}
+
+}
